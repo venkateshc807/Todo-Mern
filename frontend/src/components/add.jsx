f4735d35b9ff5549c3c0df7f743e414b9acaf6bb
venkateshc807 <cvenkatesh.bsc@gmail.com>
2025-04-09 20:14:24 +0530

    Updated port

--- frontend/src/components/add.jsx
+++ frontend/src/components/add.jsx
@@ -16,7 +16,7 @@ function Add() {
             return;
         }
 
-        axios.post('http://127.0.0.1:3001/addTodoList', { task: newTask, status: newStatus, deadline: newDeadline })
+        axios.post('http://127.0.0.1:3002/addTodoList', { task: newTask, status: newStatus, deadline: newDeadline })
             .then(res => {
                 console.log(res);
                 alert("Task added successfully!");
